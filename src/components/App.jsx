33952d425078151f6c4cfb5afaa52e79b0c11725
Dylan Lewis <dyldlewis@gmail.com>
2017-09-15 11:09:17 -0700

    create react development environment

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,7 @@ import React from "react";
 function App(props){
   return (
     <div>
-      <h3>Get to Coding!</h3>
-      <p><em>Template provided by The Dylan Lewis</em></p>
+      <p>Garden Buddy</p>
     </div>
   );
 }
