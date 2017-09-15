80738e4a9626d8e0b145f8262e0b60441d3da74b
Dylan Lewis <dyldlewis@gmail.com>
2017-09-15 12:14:34 -0700

    refactor PostInput to class based component, added form submission

--- src/components/PostInput.jsx
+++ src/components/PostInput.jsx
@@ -1,9 +1,58 @@
 import React from 'react';
+import Plant from '../models/plant'
 
+class PostInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleNewPlantFormSubmission = this.handleNewPlantFormSubmission.bind(this);
+  }
 
+  handleNewPlantFormSubmission(event) {
+    event.preventDefault();
+    const { _name } = this.refs;
+    const { _description } = this.refs;
+    var newPlant = new Plant(_name.value, _description.value);
+    this.props.onNewPlantCreation(newPlant);
+    _content.value = "";
+  }
 
-function PostInput(props) {
-  return(
-    
-  )
+  render() {
+    var inputStyle = {
+      display: "inline-block",
+      marginLeft: "50px",
+      width: "65%",
+      height: "25px"
+    }
+    var titleStyle = {
+      display: "inline-block",
+      marginLeft: "10px"
+    }
+
+    return(
+      <div>
+        <h3 style={titleStyle}>Blessed be...</h3>
+        <form onSubmit={this.handleNewPlantFormSubmission}>
+          <input
+            style={inputStyle}
+            ref="_name"
+            type="text"
+            id="name"
+            placeholder="Name"></input>
+            <input
+              style={inputStyle}
+              ref="_description"
+              type="text"
+              id="description"
+              placeholder="Description"></input>
+          <button type="submit">Add</button>
+      </form>
+      </div>
+    )
+  }
 }
+
+PostInput.proptypes = {
+  onNewPlantCreation: PropTypes.func
+}
+
+export default PostInput;
